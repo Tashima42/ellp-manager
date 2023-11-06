3455abadfe3782e9101eb8869c34bff3e32dc111
Pedro Tashima <pedro.tashima@suse.com>
2023-11-06 16:43:03 -0300

    add workshop classes table migration

---
+++ database/schema_migrations/000005_create_workshop_classes_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS workshops (
+  id INTEGER PRIMARY KEY,
+  'workshop_id' INTEGER NOT NULL,
+  'name' TEXT NOT NULL,
+  'description' TEXT,
+  'number' iNTEGER NOT NULL,
+  'start_at' DATE NOT NULL,
+  'end_at' DATE NOT NULL,
+  'created_at' DATE NOT NULL,
+  'updated_at' DATE NOT NULL,
+
+  FOREIGN KEY('workshop_id') REFERENCES 'workshops'('id')
+);
